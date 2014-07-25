e6bc22aed31f57dc806bdba87d4ae4fc567637d7
Allison Law <hsallisonlaw@gmail.com>
2014-07-25 19:23:49 +0000

    Modified index.php and article.php to read data from the authors table and to add mailto links for author names. Modified style.css to change a link colors.

--- article.php
+++ article.php
@@ -15,7 +15,11 @@ if(!$connection){
 }
 
 //Get the row for $id from the 'articles' table
-$result = mysql_query('SELECT * FROM articles WHERE id = ' . $id);
+$result = mysql_query('SELECT articles.id, articles.title, articles.author_id, authors.author_name, authors.author_email_address, articles.body ' .
+  'FROM articles LEFT JOIN authors ON articles.author_id = authors.author_id ' . 
+  'WHERE id = ' . $id);
+
+//$result = mysql_query('' . $id);
 $row = mysql_fetch_array($result);
 ?>
 
@@ -25,7 +29,7 @@ $row = mysql_fetch_array($result);
     <title>
       <?php
 
-        print 'Pirates Blog: ' . $row['title'];
+        print 'The Pirate Blog: ' . $row['title'];
 
       ?>
     </title>
@@ -42,7 +46,7 @@ $row = mysql_fetch_array($result);
       print '<h1>' . $row['title']  . '</h1>';
 
       //Prints the author
-      print '<em>By: ' . $row['author'] . '</em>';
+      print '<em>By: <a href="mailto: ' . $row['author_email_address']  . ' ">' . $row['author_name'] . '</a></em>';
 
       //Prints the article body
       print '<p>' . $row['body'] . '</p>';
@@ -50,8 +54,6 @@ $row = mysql_fetch_array($result);
       print '</article>';
 
     ?>
-
-
 
   </body>
 
--- index.php
+++ index.php
@@ -11,8 +11,9 @@ if(!$connection){
   die('Failed to connect to MySQL: ' . mysql_connect_error());
 }
 
-$result = mysql_query('SELECT id, title, author, body FROM articles ORDER BY id DESC');
-
+//$result = mysql_query('SELECT id, title, author, body FROM articles');
+//$result = mysql_query('');
+$result = mysql_query('SELECT articles.id, articles.title, authors.author_name, authors.author_email_address, articles.body FROM articles LEFT JOIN authors ON articles.author_id = authors.author_id ORDER BY id DESC');
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -24,7 +25,7 @@ $result = mysql_query('SELECT id, title, author, body FROM articles ORDER BY id 
   </head>
 
   <body>
-    <h1>Welcome to the Pirate Blog<br/> Arrr... Matey!!</h1>
+    <h1>Welcome to The Pirate Blog<br/> Arrr... Matey!!</h1>
 
     <?php
 
@@ -36,8 +37,8 @@ $result = mysql_query('SELECT id, title, author, body FROM articles ORDER BY id 
         //Prints an h2 with link for each article
         print '<h2><a href="/day-5-exercises/article.php?id=' . $row['id'] . '">'  . $row['title'] . '</a></h2>';
 
-        //Prints the author
-        print '<em>By: ' . $row['author'] . '</em>';
+        //Prints the author name with mailto link
+        print '<em>By: <a href="mailto: ' . $row['author_email_address']  . '">' . $row['author_name'] . '</a></em>';
 
         //Prints the body
         print '<p>' . $row['body'] . '</p>';
